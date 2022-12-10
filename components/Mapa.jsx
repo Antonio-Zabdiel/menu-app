b8be3953d2decfe746aad68057555cdc5fc0e784
Antonio-Zabdiel <cabrera.aguirre.antonio.zabdiel@gmail.com>
2022-12-10 14:10:35 -0600

    funciona el mapita 2 :)

--- components/Mapa.jsx
+++ components/Mapa.jsx
@@ -1,5 +1,11 @@
 import { Text, StyleSheet, View } from "react-native";
 import MapView from 'react-native-maps'
+import Circle from 'react-native-maps'
+
+var locat = {
+    latitude: 19.432899319086438, 
+    longitude: -99.1332274758037,
+};
 
 export function Mapa() {
     return(
@@ -8,13 +14,24 @@ export function Mapa() {
                 style={styles.map}
                 initialRegion={{
                     /*17.196160946616754, -108.3930000571495 */
-                    latitude: 17.196160946616754,
-                    longitude: -108.3930000571495,
+                    //latitude: 15.920185290137738,
+                    //longitude: -101.26456921311957,
+                    latitude: 19.432899319086438, 
+                    longitude: -99.1332274758037,
                     /*24.474774410434552, -96.17286459995714*/
-                    latitudeDelta: 7.278613464,
-                    longitudeDelta: 12.22013546,
+                    latitudeDelta: 10.817894458495692,
+                    longitudeDelta: 10.52724584856449,
                 }}
-            />
+            >
+                <MapView.Circle 
+                    center = {locat} 
+                    radius = {500000}
+                    strokeWidth = {1}
+                    strokeColor="#FC0101"
+                    fillColor="#f0afaf38"
+                ></MapView.Circle>
+            </MapView>
+
             <Text>
                 {"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"}
             </Text>
